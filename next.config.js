cfa558a7d82cf8423e6b87920d1a0d1923c48d8b
Kevin Li <None>
2023-12-08 20:59:33 +0800

    enable standalone

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  	images: {
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    },
-  	output: 'standalone'
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
+
+module.exports ={
+output:"standalone",
+}
