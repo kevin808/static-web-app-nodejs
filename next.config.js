c46a126a4422463dd3bc8fee78be6152392d4a55
kevin <kevin80828@gmail.com>
2023-12-09 13:23:30 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
+    dangerouslyAllowSVG: false,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   output: 'standalone'
