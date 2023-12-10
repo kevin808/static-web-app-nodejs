58a4ea3f7a56f3396bbb9d8eacbd6f0cba0a6c5c
Kevin Li <None>
2023-12-10 09:22:18 +0800

    add public

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-
   	images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'klisto.blob.core.windows.net',
-          port: '',
-          pathname: '/blobtest/**',
-        },
-      ],
       dangerouslyAllowSVG: true,
       contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
